02077f58607e7e46a14d20a97bf52b1d8e28fa51
aniket-codedater <aniketkok10@gmail.com>
2017-08-15 02:01:10 +0530

    prebuild update for 1x8100 and 1x8100 matrix of 0:90 0:90

--- math-analysis/3-link-model/data_prebuild.m
+++ math-analysis/3-link-model/data_prebuild.m
@@ -1,14 +1,17 @@
-data_number = 10000;
+data_number = 8100;
 a = 10;
 b = 20;
 c = 40;
 t0 = 45*ones(data_number,1);
-
-th(:,1) = [90/data_number:90/data_number:90];
-for i = 1:data_number
-    th(i,2) = rand()*90;
+x = [1:90]';
+t2 = x;
+for i = 1:89
+    t2 = vertcat(t2,x);
 end
-
-t1=th(:,1);
-t2=th(:,2);
+x = ones(90,1);
+t1 = x;
+for i = 2:90
+    t1 = vertcat(t1,x*i);
+end
+th = horzcat(t1,t2);
 [x,y] = kinematic_eq(a,b,c,t0,th);
