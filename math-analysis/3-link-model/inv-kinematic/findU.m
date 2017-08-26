d8faf7354b3f93339eeaf4c90c8fb503ecabfc01
Bhavik Parmar <parmarbhavik@hotmail.com>
2017-08-26 19:29:12 +0530

    Lagrangian formulation init

---
+++ math-analysis/3-link-model/inv-kinematic/findU.m
@@ -0,0 +1,18 @@
+function U = findU(A,Q,i,j,k)
+if nargin < 5
+    if j <= i
+        U = A{1,j}*Q*A{j,i+1};
+    else
+        U = 0;
+    end
+else
+    if i >= k && k >= j
+        U = A{1,j}*Q*A{j,k}*Q*A{k,i+1};
+    elseif i >= j && j >= k
+        U = A{1,k}*Q*A{k,j}*Q*A{j,i+1};
+    elseif i < j || i < k   
+        U = 0;
+    end
+end
+end
+
