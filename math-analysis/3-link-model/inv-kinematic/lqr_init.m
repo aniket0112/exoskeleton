dbb4a11502c04d7e13ac301fcef423c374c06aaa
aniket-codedater <aniketkok10@gmail.com>
2017-11-11 00:21:58 +0530

    lqr, discretization and response tests

---
+++ math-analysis/3-link-model/inv-kinematic/lqr_init.m
@@ -0,0 +1,16 @@
+A = [zeros(2,2) eye(2,2);zeros(2,2) zeros(2,2)];
+B = [zeros(2,2); eye(2,2)];
+C = [eye(4,4)];
+D = [zeros(size(B))];
+Q = [eye(4,4)];
+R = [eye(2,2)];
+x0 = [10; 10; 0; 0];
+%state space rep
+gensys = ss(A,B,C,D);
+
+[K,S,e] = lqr(gensys,Q,R);
+
+U = A-B*K;
+lqss = ss(U,[],C,[]);
+initial(lqss,x0);
+grid on;
